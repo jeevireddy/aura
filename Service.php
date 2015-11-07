2e42bb2b299976f99310f929b0044422aca6820e
jeevireddy <jeevireddy@gmail.com>
2015-11-07 20:29:54 +0530

    user id , chat group

--- Service.php
+++ Service.php
@@ -16,7 +16,7 @@ function nullif($var){
  {
  	echo "Failed to connect to MySQL: " . mysqli_connect_error();
  }
- $sql = "INSERT INTO  tbl_chathistory (Chat,Account_id,chat_ts,Latitude,Longitude) VALUES ('"	.nullif($_POST["Chat"])."',222115,sysdate(),40.91709,-72.709457)";
+ $sql = "INSERT INTO  tbl_chathistory (Chat,Account_id,chat_ts,Latitude,Longitude,user_id,chat_group) VALUES ('"	.nullif($_POST["Chat"])."',222115,sysdate(),40.91709,-72.709457,'test','nfl')";
  						if (!mysqli_query($con,$sql))
  						{
  							die('Error: ' . mysqli_error($con));
